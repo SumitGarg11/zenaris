133d6657efa0cb3e6932e262bea1bb27e5520d18
SumitGarg <garg10sumit@gmail.com>
2024-10-27 00:22:11 +0530

    commit

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,37 +1,4 @@
-// import { SignUp } from '@clerk/nextjs'
 
-// export default function Page() {
-//   return (
-//     <section className="bg-white dark:bg-gray-900">
-//     <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-      
-//       {/* Image Section - Only visible on large screens (lg and above) */}
-//       <section className="relative hidden lg:flex h-30 items-end bg-gray-100 lg:col-span-5 lg:h-full xl:col-span-6">
-//         <img
-//           src={"./image.png"}
-//           className="absolute inset-0 h-[100%]  w-full object-cover opacity-80"
-//           alt="Background"
-//         />
-          
-          
-//       </section>
-
-//       {/* Form Section - Always visible */}
-//       <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
-//         <div className="max-w-xl lg:max-w-3xl">
-
-//           {/* Logo for small and large screens */}
-          
-
-//           {/* Sign-in Form */}
-//           <SignUp />
-//         </div>
-//       </main>
-//     </div>
-//   </section>
-//   )
-  
-// }
 
 
 "use client"
